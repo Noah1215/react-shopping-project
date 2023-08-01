80f77f413ee6810fda6fca96c30a8aff34d1d3f1
Chanwoong(Noah) Park <135056537+Noah1215@users.noreply.github.com>
2023-08-01 11:41:55 -0400

    Form-Add-Product

--- src/pages/NewProduct.jsx
+++ src/pages/NewProduct.jsx
@@ -1,7 +1,106 @@
-import React from "react";
+import React, { useState } from "react";
+import Button from "../components/ui/Button";
+import { uploadImage } from "../api/uploader";
+import { addNewProduct } from "../api/firebase";
 
 const NewProduct = () => {
-  return <div>NewProduct</div>;
+  const [product, setProduct] = useState({});
+  const [file, setFile] = useState();
+  const [isUploading, setIsUploading] = useState(false);
+  const [success, setSuccess] = useState();
+
+  const handleChange = (e) => {
+    const { name, value, files } = e.target;
+    if (name === "file") {
+      setFile(files && files[0]);
+      return;
+    }
+    setProduct((product) => ({
+      ...product,
+      [name]: value,
+    }));
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setIsUploading(true);
+    uploadImage(file)
+      .then((url) => {
+        addNewProduct(product, url).then(() => {
+          setSuccess("New Product is successfully added.");
+          setTimeout(() => {
+            setSuccess(null);
+          }, 4000);
+        });
+      })
+      .finally(() => setIsUploading(false));
+  };
+  return (
+    <section className="w-full text-center">
+      <h2 className="text-2xl font-bold my-4">Add New Product</h2>
+      {success && <p className="my-2">{success}</p>}
+      {file && (
+        <img
+          className="w-96 mx-auto mb-2"
+          src={URL.createObjectURL(file)}
+          alt="file"
+        />
+      )}
+      <form className="flex flex-col px-12" onSubmit={handleSubmit}>
+        <input
+          type="file"
+          accept="image/*"
+          name="file"
+          required
+          onChange={handleChange}
+        />
+        <input
+          type="text"
+          name="title"
+          value={product.title ?? ""}
+          placeholder="Product Title"
+          required
+          onChange={handleChange}
+        />
+        <input
+          type="number"
+          name="price"
+          value={product.price ?? ""}
+          placeholder="Price"
+          required
+          onChange={handleChange}
+        />
+        <input
+          type="text"
+          name="category"
+          value={product.category ?? ""}
+          placeholder="Category"
+          required
+          onChange={handleChange}
+        />
+        <input
+          type="text"
+          name="description"
+          value={product.description ?? ""}
+          placeholder="Description"
+          required
+          onChange={handleChange}
+        />
+        <input
+          type="text"
+          name="options"
+          value={product.options ?? ""}
+          placeholder="Options(seperate with comma(,))"
+          required
+          onChange={handleChange}
+        />
+        <Button
+          text={isUploading ? "Uploading..." : "Register"}
+          disabled={isUploading}
+        />
+      </form>
+    </section>
+  );
 };
 
 export default NewProduct;
