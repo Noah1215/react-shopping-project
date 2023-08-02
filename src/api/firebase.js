01ebbeacce55bb8f23df2b76893c1f354bddb133
Chanwoong(Noah) Park <135056537+Noah1215@users.noreply.github.com>
2023-08-02 18:46:24 -0400

    ENV3

--- src/api/firebase.js
+++ src/api/firebase.js
@@ -10,10 +10,10 @@ import {
 import { getDatabase, ref, set, get, remove } from "firebase/database";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAlGp4KUzNK9cew6IbbZ2aKuACm8N5XSqw",
-  authDomain: "snoppy-69dad.firebaseapp.com",
-  databaseURL: "https://snoppy-69dad-default-rtdb.firebaseio.com",
-  projectId: "snoppy-69dad",
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_FIREBASE_DB_URL,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
 };
 
 const app = initializeApp(firebaseConfig);
